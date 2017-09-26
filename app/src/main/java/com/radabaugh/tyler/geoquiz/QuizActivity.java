7d1dbd26c7ecdea7b591b47edd3a7a1002d011c9
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-09-26 11:10:32 -0700

    Chap 1 pg 21, Adding member variables.

--- app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
+++ app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
@@ -2,8 +2,13 @@ package com.radabaugh.tyler.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
