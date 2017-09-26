56186795169679c7e8c3696a2784c6a34ab1316b
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-09-26 12:22:28 -0700

    Revert "Chap 1 pg 32, Challenge: Customizing the Toast."
    This reverts commit cce2da437c618e1aed22ff54288692bd93bf2164.

--- app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
+++ app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
@@ -2,7 +2,6 @@ package com.radabaugh.tyler.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,23 +21,18 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(QuizActivity.this,
+                Toast.makeText(QuizActivity.this,
                                R.string.correct_toast,
-                               Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 500);
-                toast.show();
-
+                               Toast.LENGTH_SHORT).show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(QuizActivity.this,
+                Toast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 500);
-                toast.show();
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
