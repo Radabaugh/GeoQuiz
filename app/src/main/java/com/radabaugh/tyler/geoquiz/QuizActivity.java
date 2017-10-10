a2dafc0528624b218d9df436719edbf2aff46604
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-10 13:23:36 -0700

    Chap 5 pg 98,99 Adding a cheat button to QuizActivity

--- app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
+++ app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
@@ -14,6 +14,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
@@ -63,6 +64,14 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity
             }
         });
 
