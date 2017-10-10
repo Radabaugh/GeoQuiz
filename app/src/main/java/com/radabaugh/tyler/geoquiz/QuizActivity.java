7ae40cc5cf6d7ac770da5c0b0b7c3732c7c49bc3
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-10 11:26:01 -0700

    Chap 4 pg 78, Logging stack traces

--- app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
+++ app/src/main/java/com/radabaugh/tyler/geoquiz/QuizActivity.java
@@ -107,6 +107,7 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestion() {
+        Log.d(TAG, "Updating question text", new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
